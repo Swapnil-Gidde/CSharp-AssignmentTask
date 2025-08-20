d73af92407a576b82ad762b42d2d04f897e7baf8
Swapnil-Gidde <swapnilgidde2002@gmail.com>
2025-08-20 22:03:26 +0530

    First commit with projects, ignored VS/bin/obj files

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,379 @@
+﻿//class Product
+//{
+//    public int ProductId { get; set; }
+//    public string ProductName { get; set; }
+
+//    public Product()
+//    {
+//        ProductId = 0;
+//        ProductName = "unknown";
+//    }
+//    public Product(int ProductId, string ProductName)
+//    {
+//        this.ProductId = ProductId;
+//        this.ProductName = ProductName;
+//    }
+//    public override string ToString()
+//    {
+//        return $"Product[Productid = {ProductId} , ProductName= {ProductName}]";
+//    }
+//}
+//class Electronics : Product
+//{
+//    public string WarentyYear { get; set; }
+
+//    public Electronics()
+//    {
+//        WarentyYear = "unknown";
+//    }
+//    public Electronics(int ProductId, string ProductName, string WarentyYear) : base(ProductId, ProductName)
+//    {
+//        this.WarentyYear = WarentyYear;
+//    }
+
+
+//    public override string ToString()
+//    {
+//        return $" {base.ToString()}Electronics[WarentyYear= {WarentyYear}]";
+//    }
+//}
+
+//class Smartphone : Electronics
+//{
+//    public string OS { get; set; }
+//    public Smartphone()
+//    {
+//        OS = "unknown";
+//    }
+//    public Smartphone(int ProductId, string ProductName, string WarentyYear, string OS) : base(ProductId, ProductName, WarentyYear)
+//    {
+//        this.OS = OS;
+//    }
+
+//    public override string ToString()
+//    {
+//        return $"{base.ToString()}Smartphone[Os= {OS}]";
+//    }
+//}
+//class App
+//{
+//    static void Main(string[] args)
+//    {
+//        //Smartphone smartphone = new Smartphone();
+//        //smartphone.ElectronicsDetails();
+
+//        Smartphone sm = new Smartphone(1, "Samsung", "2Yrs", "11");
+//        Console.WriteLine(sm);
+//    }
+//}
+
+
+////1] Single Inheritance
+//class Vehicle
+//{
+//    public string Brand {  get; set; }
+//    public int Speed {  get; set; }
+
+//    public Vehicle()
+//    {
+
+//    }
+//    public Vehicle(string brand, int speed)
+//    {
+//        this.Brand = brand;
+//        this.Speed = speed;
+//    }
+//    public void VehicleDetails()
+//    {
+//        Console.WriteLine($"Brand is {this.Brand} , Speed is {this.Speed}");
+//    }
+//    public override string ToString()
+//    {
+//        return $"Vehicle Brand is {Brand} , Speed is {Speed}";
+//    }
+//}
+//class Car : Vehicle
+//{
+//    public int Mileage {  get; set; }
+
+//    public Car() 
+//    {
+
+//    }
+//    public Car(string brand , int speed ,int Mileage) : base(brand , speed)
+//    {
+//        this.Mileage = Mileage;
+//    }
+
+//    public void CarDetails()
+//    {
+//        Console.WriteLine($"Mileage is {this.Mileage}");
+//    }
+//    public override string ToString()
+//    {
+//        return $"{base.ToString()}, Mileage is {Mileage}";
+//    }
+//}
+//class SimpleInheritance
+//{
+//    static void Main(string[] args)
+//    {
+//        Car c = new Car("Toyota", 34, 9);
+//        c.Mileage = 10;
+//        Console.WriteLine(c);
+
+//        // with methods
+//        c.VehicleDetails();
+//        c.CarDetails();
+//    }
+//}
+
+////  Why base constructor call 
+//// 2] Multilevel Inheritance
+//class Person
+//{
+//    public string Name { get; set; }
+//    public int Age {  get; set; }
+
+//    public Person()
+//    {
+//        Name = "Unknown";
+//        Age = 0;
+//    }
+//    public Person(string Name , int Age)
+//    {
+//        this.Name = Name;
+//        this.Age = Age;
+//    }
+//    public override string ToString()
+//    {
+//        return $"Person name is {this.Name} , age is {this.Age}";
+//    }
+//}
+//class Employee : Person
+//{
+//    public int Salary {  get; set; }
+//    public int Id {  get; set; }
+//    public Employee()
+//    { 
+//        Salary = 0;
+//        Id = 0;
+//    }
+//    public Employee(string Name , int Age, int Salary , int Id) : base(Name , Age)
+//    {
+//        this.Salary = Salary;
+//        this.Id = Id;
+//    }
+
+//    public override string ToString()
+//    {
+//        return $"{base.ToString()}, Salary = {this.Salary} , Id = {this.Id}";
+//    }
+//}
+//class Manager : Employee
+//{
+//    public string Department {  get; set; }
+
+//    //public Manager()
+//    //{
+//    //    Department = "unknown";
+
+//    //}
+//    //public Manager(String Name, int Age , int Salary , int Id, string Department) : base(Name , Age , Salary , Id)
+//    //{
+//    //    this.Department = Department;
+//    //}
+//    public override string ToString()
+//    {
+//        return $"{base.ToString()} , department = {Department}";
+//    }
+//}
+//class MultilevelInheritance
+//{
+//    public static void Main(string[] args)
+//    {
+//        Manager m = new Manager();
+//        Console.WriteLine(m);
+
+//        //Manager m1 = new Manager("Ram" , 23,30000, 1, "Developer");
+//        Manager m1 = new Manager { Name = "Ram", Age = 23, Salary = 30000, Id = 1, Department = "Developer" };
+//        m1.Salary = 40000;
+//        Console.WriteLine(m1);
+//    }
+//}
+
+// 3] 1) Hierachical Inheritance
+//using System.Drawing;
+
+//class Shape
+//{
+//    public string Color {  get; set; }
+
+//    public Shape()
+//    {
+
+//    }
+//    public Shape(string Color) 
+//    {
+//        this.Color = Color;
+//    }
+
+//    public override string ToString()
+//    {
+//        return $"Color = {this.Color}";
+//    }
+//}
+//class Rectangle : Shape
+//{
+//    public int Length {  get; set; }
+//    public int Width { get; set; }
+
+//    public Rectangle()
+//    {
+//        Length = 0;
+//        Width = 0;
+//    }
+
+//    public Rectangle(string color, int length , int width) : base (color)
+//    {
+//        this.Length = length;
+//        this.Width = width;
+//    }
+
+//    public void AreaDisplay()
+//    {
+//        Console.WriteLine($" Area is:- {this.Length * this.Width}");
+//    }
+//    public override string ToString()
+//    {
+//        return $"{base.ToString()} , Length = {Length} , width = {Width}";
+//    }
+//}
+//class Circle : Shape
+//{
+//    public double radius {  get; set; }
+
+//    public Circle()
+//    { 
+//        radius = 0;
+//    }
+//    public Circle(string color, double radius) : base(color)
+//    {
+//        this.radius = radius;
+//    }
+
+//    public override string ToString()
+//    {
+//        return $"{base.ToString()} , radius = {radius}";
+//    }
+//}
+//class HierarchicalInheritance
+//{
+//    public static void Main(string[] args)
+//    {
+//        Rectangle r = new Rectangle { Color = "Red" , Length = 20 , Width = 30};
+//        r.AreaDisplay();
+//        Console.WriteLine(r);
+
+//        Circle c = new Circle("White" , 3.14);
+//        Console.WriteLine(c);
+//    }
+//}
+
+// 4] Hybrid Inheritance
+class Vehicle // base class
+{
+    public string Brand { get; set; }
+
+    public Vehicle()
+    {
+
+    }
+    public Vehicle(string Brand)
+    {
+        this.Brand = Brand;
+    }
+
+    public override string ToString()
+    {
+        return $"Brand:- {this.Brand}";
+    }
+}
+class Car : Vehicle // child class of vehicle
+{
+    public int Seats { get; set; }
+
+    public Car()
+    {
+
+    }
+    public Car(string Brand, int Seats) : base(Brand)
+    {
+        this.Seats = Seats;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Seats:- {this.Seats}";
+    }
+}
+class Truck : Vehicle // child class of vehicle
+{
+    public int Capacity { get; set; }
+
+    public Truck()
+    {
+
+    }
+
+    public Truck(string Brand, int Capacity) : base(Brand)
+    {
+        this.Capacity = Capacity;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Capacity:- {this.Capacity}";
+    }
+}
+class ElectricCar : Car // grandchild of Vehicle
+{
+    public string Battery { get; set; }
+
+    public ElectricCar()
+    {
+
+    }
+
+    public ElectricCar(string Brand, int Seats, string Battery) : base(Brand, Seats)
+    {
+        this.Battery = Battery;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Battery:- {Battery}";
+    }
+}
+class HybridInheritance
+{
+    public static void Main(string[] args)
+    {
+        Vehicle v = new Vehicle("Hybrid Car");
+        Console.WriteLine(v);
+        Console.WriteLine();
+
+        Car c = new Car("Toyota", 5);
+        Console.WriteLine(c);
+        Console.WriteLine();
+
+        Truck t = new Truck("Volvo", 12);
+        Console.WriteLine(t);
+        Console.WriteLine();
+
+        ElectricCar e = new ElectricCar("TATA", 4, "100 kWh");
+        Console.WriteLine(e);
+
+
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,289 @@
+﻿// 1] Compiletime polymorphism[Method Overoading]
+
+// eg 1 
+//class Calculate
+//{
+//    public static double pi = 3.14;
+//    public double Area(double radius)
+//    {
+//        return pi * radius * radius; // circle
+//    }
+
+//    public int Area(int side)
+//    {
+//        return side * side; // square
+//    }
+
+//    public double Area(float length ,double breadth)
+//    {
+//        return length * breadth; // rectangle
+//    }
+
+//    public double Area(double base, int height )
+//    {
+//        return  1/2 * base * height; // triangle
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Calculate c = new Calculate();
+//        Console.WriteLine("Area of Circle:- " + c.Area(10.7)); // (10)
+//        Console.WriteLine("Area of Rectangle:- " + c.Area(18.5f , 5.6));
+//        Console.WriteLine("Area of Square:- " + c.Area(10f,4f));
+//        Console.WriteLine("Area of Triangle:- " + c.Area(10,4.6));
+//    }
+//}
+
+
+// ------------------------------------------------------------------------------------------------------------------------------//
+
+
+// eg 2 Compiletime polymorphism [Method overloading]
+//using System;
+
+//class Calculate
+//{
+//    public static double pi = 3.14;
+
+//    public double Area(double radius)
+//    {
+//        return pi * radius * radius; // circle
+//    }
+
+//    public int Area(int side)
+//    {
+//        return side * side; // square
+//    }
+
+//    public double Area(float length, double breadth)
+//    {
+//        return length * breadth; // rectangle
+//    }
+
+//    public double Area(double b, double height)
+//    {
+//        return 0.5 * b * height; // triangle
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Calculate c = new Calculate();
+
+//        Console.WriteLine("PI value is:- " + Calculate.pi);
+//        Console.WriteLine("Area of Circle: " + c.Area(10.7));            
+//        Console.WriteLine("Area of Rectangle: " + c.Area(18.5f, 5.6));   
+//        Console.WriteLine("Area of Square: " + c.Area(10));              
+//        Console.WriteLine("Area of Triangle: " + c.Area(10, 4.6));       
+//    }
+//}
+
+
+// ------------------------------------------------------------------------------------------------------------------------------//
+
+// Task 1
+//using System;
+
+//class InterestCalculator
+//{
+//    public double Principal { get; set; }
+//    public double Rate { get; set; }
+//    public int Year { get; set; }
+//    public int CompoundYear { get; set; }
+
+//    public void CalculateInterest(double principal, double rate)
+//    {
+//        this.Principal = principal;
+//        this.Rate = rate;
+//        Console.WriteLine($"Simple Interest: {this.Principal * this.Rate / 100}");
+//    }
+//    public void CalculateInterest(double principal, double rate, int year)
+//    {
+//        this.Principal = principal;
+//        this.Rate = rate;
+//        this.Year = year;
+//        Console.WriteLine($"Simple Interest for {year} years: {(principal * rate * year) / 100}");
+//    }
+//    public void CalculateInterest(double principal, double rate, int year, int compoundPerYear)
+//    {
+//        this.Principal = principal;
+//        this.Rate = rate;
+//        this.Year = year;
+//        this.CompoundYear = compoundPerYear;
+
+//        double compoundInterest = principal * Math.Pow(1 + (rate / (100.0 * compoundPerYear)), compoundPerYear * year) - principal;
+
+//        Console.WriteLine($"Compound Interest for {year} years: {compoundInterest}");
+//    }
+//}
+
+//class InterestApp
+//{
+//    static void Main(string[] args)
+//    {
+//        InterestCalculator ic = new InterestCalculator();
+
+//        ic.CalculateInterest(2000, 4);               
+//        ic.CalculateInterest(2000, 4, 3);            
+//        ic.CalculateInterest(2000, 4, 3, 4);         
+//    }
+//}
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------//
+
+// Task 1
+//class SalaryCalculator
+//{
+//    public int Salary { get; set; }
+//    public int Bonus { get; set; }
+//    public int Deduction { get; set; }
+//    public void CalculateSalary(int salary)
+//    {
+//        this.Salary = salary;
+//        Console.WriteLine($"Total Salary is {salary}");
+//    }
+//    public void CalculateSalary(int salary , int bonus)
+//    {
+//        this.Bonus = bonus;
+//        this.Salary =  salary ;
+//        Console.WriteLine($"Total Salary + Bonus :- {salary + bonus}");
+//    }
+//    public void CalculateSalary(int salary , int bonus,int deducation)
+//    {
+//        this.Salary = salary;
+//        this.Bonus = bonus;
+//        this.Deduction = deducation;
+//        Console.WriteLine($"Salary with deduction: {Salary + Bonus  - deducation} ");
+//    }
+//}
+//class SalaryApp
+//{
+//    static void Main(string[] args)
+//    {
+//        SalaryCalculator s = new SalaryCalculator();
+//        s.CalculateSalary(9000);
+//        s.CalculateSalary(100 ,34);
+//        s.CalculateSalary(50 , 89,60);
+//    }
+//}
+
+
+// ------------------------------------------------------------------------------------------------------------------------------//
+
+// Task 1 , only one argument pass, not everytime salary , bonus pass
+
+//class SalaryCalculator
+//{
+//    public double Salary { get; set; }
+//    public float Bonus { get; set; }
+//    public int Deduction { get; set; }
+
+//    public void CalculateSalary(double salary)
+//    {
+//        this.Salary = salary;
+//        Console.WriteLine($"Total Salary is {salary}");
+//    }
+
+//    public void CalculateSalary(float bonus)
+//    {
+//        this.Bonus = bonus;
+//        Console.WriteLine($"Total Salary + Bonus: {this.Salary + bonus}");
+//    }
+
+//    public void CalculateSalary(int deduction)
+//    {
+//        this.Deduction = deduction;
+//        Console.WriteLine($"Salary with Deduction: {(this.Salary + this.Bonus) - deduction}");
+//    }
+//}
+//class SalaryApp
+//{
+//    static void Main(string[] args)
+//    {
+//        SalaryCalculator s = new SalaryCalculator();
+
+//        s.CalculateSalary(9000.00);
+//        s.CalculateSalary(100.8f);
+//        s.CalculateSalary(50);
+
+//    }
+//}
+
+
+// ------------------------------------------------------------------------------------------------------------------------------//
+// 2] Runtime polymorphism
+using System.Runtime.Intrinsics.Arm;
+
+class Shape
+{
+    public virtual void Area()
+    {
+        Console.WriteLine("Area ");
+    }
+}
+class Rectangle : Shape
+{
+    public int width;
+    public int height;
+    public Rectangle()
+    {
+
+    }
+    public Rectangle(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public override void Area()
+    {
+        Console.WriteLine($"Area of Rectangle:- {this.width * this.height}");
+    }
+}
+class Circle : Shape
+{
+    public float radius;
+
+    public Circle()
+    {
+
+    }
+    public Circle(float radius)
+    {
+        this.radius = radius;
+    }
+
+    public override void Area()
+    {
+        Console.WriteLine($"Area of Circle:- {3.14 * this.radius * this.radius}");
+    }
+}
+class Polymorphism
+{
+    static void Main(string[] args)
+    {
+
+        //Shape s = new Rectangle(23, 56);
+        //s.Area();
+
+        //Shape s2 = new Circle(4);
+        //s2.Area();
+
+
+        Shape s; // parent reference 
+        s = new Rectangle(23, 56); // parent reference and child object
+        s.Area();
+
+        s = new Circle(4); // parent reference and child object
+        s.Area();
+    }
+
+
+}
+
